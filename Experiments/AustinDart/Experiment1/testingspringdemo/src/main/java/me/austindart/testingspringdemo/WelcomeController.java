da2aed20693aea6f0dd62dbe961bc71f661316fc
Austin Dart <aedart@iastate.edu>
2021-09-06 20:42:24 -0500

    Update WelcomeController.java

--- Experiments/AustinDart/Experiment1/testingspringdemo/src/main/java/me/austindart/testingspringdemo/WelcomeController.java
+++ Experiments/AustinDart/Experiment1/testingspringdemo/src/main/java/me/austindart/testingspringdemo/WelcomeController.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 class WelcomeController {
 
+    // This is based off of the tutorial class. I made a few small changes.
+
     @GetMapping("/")
-    public String welcome() {
+    public String message() {
         return "I am Austin Dart. I am trying Springboot.";
     }
 
     @GetMapping("/{name}")
-    public String welcome(@PathVariable String name) {
+    public String message(@PathVariable String name) {
         return "I am Austin Dart. I am trying Springboot. Your name is " + name;
     }
 }
--- Experiments/AustinDart/Test.java
+++
@@ -1,8 +0,0 @@
-package AustinDart;
-
-public class Test {
-
-    public static void main(String[] args){
-        // testing Git and Java in Experiment2 Studio
-    }
-}
