656878a4e91b3e2af581287154aeb113289940ec
kyletodd <kyletodd@iastate.edu>
2021-09-26 15:28:03 -0500

    Added FirstApp Test to experiments

--- Experiments/KyleTodd/app/src/main/java/edu/iastate/kyletodd/MainActivity.java
+++ Experiments/KyleTodd/app/src/main/java/edu/iastate/kyletodd/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         user = (EditText)findViewById(R.id.uName);
         password = (EditText)findViewById(R.id.uPassword);
         login = (Button)findViewById(R.id.buttonLogin);
+        TextView here = findViewById(R.id.registerHere);
+
+        here.setOnClickListener(new View.OnClickListener(){
+           @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, registerHere.class));
+            }
+        });
+
 
         login.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -33,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void register(String userName, String userPassword){
         if((userName == "kyletodd") && (userPassword == "password"));
-        Intent i = new Intent(MainActivity.this, newActivity.class);
-        startActivity(i);
+            Intent i = new Intent(MainActivity.this, newActivity.class);
+            startActivity(i);
+
     }
+
 }
