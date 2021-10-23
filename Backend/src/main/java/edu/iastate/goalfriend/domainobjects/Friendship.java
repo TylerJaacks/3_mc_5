0f927768bbc745100a38b0ddd2cbf67818a500ef
Austin Dart <aedart@iastate.edu>
2021-10-23 17:04:02 -0500

    Add functionality to Friendship.java and Goal.java

--- Backend/src/main/java/edu/iastate/goalfriend/domainobjects/Friendship.java
+++ Backend/src/main/java/edu/iastate/goalfriend/domainobjects/Friendship.java
@@ -5,6 +5,7 @@ import edu.iastate.goalfriend.FriendshipType;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
 @Table(name = "friendship")
@@ -17,4 +18,59 @@ public class Friendship {
     private @NotBlank @NotNull @OneToOne(cascade=CascadeType.ALL) User user1;
     private @NotBlank @NotNull @OneToOne(cascade=CascadeType.ALL) User user2;
     private @NotBlank @NotNull FriendshipType friendshipType;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public FriendshipType getFriendshipType(){
+        return friendshipType;
+    }
+
+    public void setFriendshipType(FriendshipType friendshipType){
+        this.friendshipType = friendshipType;
+    }
+
+    public User getUser1() {
+        return user1;
+    }
+
+    public void setUser1(User user1) {
+        this.user1 = user1;
+    }
+
+    public User getUser2() {
+        return user2;
+    }
+
+    public void setUser2(User user2) {
+        this.user2 = user2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Friendship)) return false;
+        Friendship that = (Friendship) o;
+        return getId() == that.getId() && getUser1().equals(that.getUser1()) && getUser2().equals(that.getUser2()) && getFriendshipType() == that.getFriendshipType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUser1(), getUser2(), getFriendshipType());
+    }
+
+    @Override
+    public String toString() {
+        return "Friendship{" +
+                "id=" + id +
+                ", user1=" + user1 +
+                ", user2=" + user2 +
+                ", friendshipType=" + friendshipType +
+                '}';
+    }
 }
--- Backend/src/main/java/edu/iastate/goalfriend/domainobjects/Goal.java
+++ Backend/src/main/java/edu/iastate/goalfriend/domainobjects/Goal.java
@@ -5,6 +5,7 @@ import edu.iastate.goalfriend.GoalCategory;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
 @Table(name = "goals")
@@ -19,4 +20,70 @@ public class Goal {
     private @NotBlank @NotNull User goalOwner;
 
     private @NotBlank @NotNull GoalCategory goalCategory;
+
+    private @NotBlank @NotNull float goalProgress;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGoalName() {
+        return goalName;
+    }
+
+    public void setGoalName(String goalName) {
+        this.goalName = goalName;
+    }
+
+    public User getGoalOwner() {
+        return goalOwner;
+    }
+
+    public void setGoalOwner(User goalOwner) {
+        this.goalOwner = goalOwner;
+    }
+
+    public GoalCategory getGoalCategory() {
+        return goalCategory;
+    }
+
+    public void setGoalCategory(GoalCategory goalCategory) {
+        this.goalCategory = goalCategory;
+    }
+
+    public float getGoalProgress(){
+        return goalProgress;
+    }
+
+    public void setGoalProgress(float goalProgress){
+        this.goalProgress = goalProgress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Goal)) return false;
+        Goal goal = (Goal) o;
+        return getId() == goal.getId() && Float.compare(goal.getGoalProgress(), getGoalProgress()) == 0 && getGoalName().equals(goal.getGoalName()) && getGoalOwner().equals(goal.getGoalOwner()) && getGoalCategory() == goal.getGoalCategory();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getGoalName(), getGoalOwner(), getGoalCategory(), getGoalProgress());
+    }
+
+    @Override
+    public String toString() {
+        return "Goal{" +
+                "id=" + id +
+                ", goalName='" + goalName + '\'' +
+                ", goalOwner=" + goalOwner +
+                ", goalCategory=" + goalCategory +
+                ", goalProgress=" + goalProgress +
+                '}';
+    }
 }
