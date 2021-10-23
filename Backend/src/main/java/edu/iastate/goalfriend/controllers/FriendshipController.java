aa710582a5682304aa3ea25b0ace18cd3f402a92
Tyler Jaacks <TylerJaacks@Gmail.com>
2021-10-23 18:03:59 -0500

    Finished Friendship

--- Backend/src/main/java/edu/iastate/goalfriend/controllers/FriendshipController.java
+++ Backend/src/main/java/edu/iastate/goalfriend/controllers/FriendshipController.java
@@ -5,10 +5,8 @@ import edu.iastate.goalfriend.constants.ErrorConstants;
 import edu.iastate.goalfriend.domainobjects.Friendship;
 import edu.iastate.goalfriend.domainobjects.Token;
 import edu.iastate.goalfriend.domainobjects.User;
-import edu.iastate.goalfriend.exceptions.FriendshipAlreadyExistException;
-import edu.iastate.goalfriend.exceptions.InvalidHeadersException;
-import edu.iastate.goalfriend.exceptions.TokenHasExpiredException;
-import edu.iastate.goalfriend.exceptions.UserDoesNotExistException;
+import edu.iastate.goalfriend.exceptions.*;
+import edu.iastate.goalfriend.reponses.FriendshipsResponse;
 import edu.iastate.goalfriend.reponses.IResponse;
 import edu.iastate.goalfriend.reponses.SuccessResponse;
 import edu.iastate.goalfriend.repositories.FriendshipRepository;
@@ -19,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -82,20 +81,70 @@ public class FriendshipController {
     }
 
     @DeleteMapping(path = "/friendship", produces = MediaType.APPLICATION_JSON_VALUE)
-    public IResponse removeFriendship(@RequestHeader String token, @RequestHeader String otherUsername) throws InvalidHeadersException {
+    public IResponse removeFriendship(@RequestHeader String token, @RequestHeader String otherUsername) throws InvalidHeadersException, TokenHasExpiredException, UserDoesNotExistException, FriendshipAlreadyExistException, FriendshipDoesNotExistException {
         if (token == null || otherUsername == null || token.isEmpty() || otherUsername.isEmpty()) {
             throw new InvalidHeadersException(ErrorConstants.ERROR_CODE_INVALID_HEADERS, "Invalid headers were supplied.");
         }
 
-        return null;
+        Token userToken = tokenRepository.getByToken(token);
+
+        if (!TokenUtils.isTokenValid(userToken, token)) {
+            throw new TokenHasExpiredException(ErrorConstants.ERROR_CODE_TOKEN_EXPIRED, "Token has expired.");
+        }
+
+        User user = userRepository.findByToken_Token(token);
+
+        if (user == null) {
+            throw new UserDoesNotExistException(ErrorConstants.ERROR_CODE_USER_DOESNT_EXIST, "This user doesn't exist.");
+        }
+
+        User otherUser = userRepository.findByUsername(otherUsername);
+
+        if (otherUser == null) {
+            throw new UserDoesNotExistException(ErrorConstants.ERROR_CODE_USER_DOESNT_EXIST, "This user doesn't exist.");
+        }
+
+        List<Friendship> usersFriends = friendshipRepository.getAllByUser1UsernameEquals(user.getUsername());
+
+        for (Friendship friendship : usersFriends) {
+            if (friendship.getUser2().getUsername().equals(otherUsername)) {
+                friendshipRepository.delete(friendship);
+                friendshipRepository.save(friendship);
+            }
+        }
+
+        List<Friendship> otherUsersFriends = friendshipRepository.getAllByUser1UsernameEquals(otherUser.getUsername());
+
+        for (Friendship friendship : otherUsersFriends) {
+            if (friendship.getUser2().getUsername().equals(user)) {
+                friendshipRepository.delete(friendship);
+                friendshipRepository.save(friendship);
+            }
+        }
+
+        throw new FriendshipDoesNotExistException(ErrorConstants.ERROR_CODE_FRIENDSHIP_DOESNT_EXISTS, "You are already friends with this user.");
     }
 
     @GetMapping(path = "/friendship", produces = MediaType.APPLICATION_JSON_VALUE)
-    public IResponse getFriendships(@RequestHeader String token) throws InvalidHeadersException {
+    public IResponse getFriendships(@RequestHeader String token) throws InvalidHeadersException, TokenHasExpiredException {
         if (token == null || token.isEmpty()) {
             throw new InvalidHeadersException(ErrorConstants.ERROR_CODE_INVALID_HEADERS, "Invalid headers were supplied.");
         }
 
-        return null;
+        Token userToken = tokenRepository.getByToken(token);
+
+        if (!TokenUtils.isTokenValid(userToken, token)) {
+            throw new TokenHasExpiredException(ErrorConstants.ERROR_CODE_TOKEN_EXPIRED, "Token has expired.");
+        }
+
+        User user = userRepository.findByToken_Token(token);
+
+        List<User> friends = new ArrayList<>();
+
+        friendshipRepository.getAllByUser1UsernameEquals(user.getUsername()).stream().forEach(f -> {
+            friends.add(f.getUser1());
+        });
+
+        return new FriendshipsResponse(friends);
     }
 }
