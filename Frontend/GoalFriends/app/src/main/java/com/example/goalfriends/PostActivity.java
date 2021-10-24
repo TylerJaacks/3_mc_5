5918c52874e8aa0f6ade1a37e45b9b264b7beee0
Elijah <elijahb@iastate.edu>
2021-10-23 19:31:34 -0500

    Added code for Goal Category drop down menu and made the post new goal cancel button work

--- Frontend/GoalFriends/app/src/main/java/com/example/goalfriends/PostActivity.java
+++ Frontend/GoalFriends/app/src/main/java/com/example/goalfriends/PostActivity.java
@@ -2,7 +2,14 @@ package com.example.goalfriends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        Spinner spinner = (Spinner) findViewById(R.id.GoalCategorySpinner);
+        ImageButton cancelButton = (ImageButton) findViewById(R.id.postGoalCancelButton);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PostActivity.this, "Post New Goal Canceled", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(PostActivity.this, HomescreenActivity.class));
+
+            }
+        });
     }
 }
