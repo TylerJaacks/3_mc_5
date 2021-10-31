054e67344af3cde519ff52c08958883ae6af06ff
Elijah <elijahb@iastate.edu>
2021-10-31 14:25:09 -0500

    Started work on the Search activity

--- Frontend/GoalFriends/app/src/main/java/com/example/goalfriends/SearchActivity.java
+++ Frontend/GoalFriends/app/src/main/java/com/example/goalfriends/SearchActivity.java
@@ -2,13 +2,68 @@ package com.example.goalfriends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+import android.app.SearchManager;
+import android.widget.SearchView.OnQueryTextListener;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private SearchView searchView;
+    private ListView listView;
+    private ArrayList<String> list;
+    private ArrayAdapter<String > adapter;
+    private ImageButton cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchView = (SearchView) findViewById(R.id.searchView);
+        listView = (ListView) findViewById(R.id.resultslistView);
+        cancelButton = (ImageButton) findViewById(R.id.searchCancelButton);
+
+
+        list = new ArrayList<>();
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SearchActivity.this, "Search Canceled", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(SearchActivity.this, HomescreenActivity.class));
+
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                if (list.contains(query)){
+                    adapter.getFilter().filter(query);
+                } else {
+                    Toast.makeText(SearchActivity.this, "No Match found",Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 }
