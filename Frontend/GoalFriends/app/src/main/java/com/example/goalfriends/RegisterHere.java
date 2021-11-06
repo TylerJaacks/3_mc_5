45b1eca0f002b5a6c6747b9aa10a70d9fe435c64
Tyler Jaacks <TylerJaacks@Gmail.com>
2021-11-05 19:12:17 -0500

    Clean up some code.

--- Frontend/GoalFriends/app/src/main/java/com/example/goalfriends/RegisterHere.java
+++ Frontend/GoalFriends/app/src/main/java/com/example/goalfriends/RegisterHere.java
@@ -1,9 +1,9 @@
 package com.example.goalfriends;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
@@ -28,24 +25,26 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
-public class registerHere extends AppCompatActivity {
+public class RegisterHere extends AppCompatActivity {
 
+    private static final String TAG = "registerHere";
     private Button cancel;
     private Button createAccount;
     private EditText username;
     private EditText email;
     private EditText phone;
     private EditText password;
-    private EditText cpassword;
-    private int statusCode;
-    private static final String TAG = "registerHere";
+    private EditText confirmPassword;
     private SharedPreferencesHelper mSharedPreferencesHelper;
     private ValidEmail mEmailValidator;
     private ValidPhoneNumber mPhoneValidator;
     private ValidPassword mPasswordValidator;
+
+    private int statusCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +57,7 @@ public class registerHere extends AppCompatActivity {
         email = (EditText) findViewById(R.id.SetEmailAddress);
         phone = (EditText) findViewById(R.id.editTextPhone);
         password = (EditText) findViewById(R.id.SetPassword);
-        cpassword = (EditText) findViewById(R.id.ConfirmPassword);
+        confirmPassword = (EditText) findViewById(R.id.ConfirmPassword);
 
         mEmailValidator = new ValidEmail();
         email.addTextChangedListener(mEmailValidator);
@@ -73,51 +72,41 @@ public class registerHere extends AppCompatActivity {
         mSharedPreferencesHelper = new SharedPreferencesHelper(sharedPreferences);
         populateUi();
 
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(registerHere.this, MainActivity.class));
+        cancel.setOnClickListener(v -> startActivity(new Intent(RegisterHere.this, MainActivity.class)));
+
+
+        createAccount.setOnClickListener(v -> {
+
+            register(username.getText().toString().trim(), email.getText().toString().trim(), phone.getText().toString().trim(),
+                    password.getText().toString().trim(), confirmPassword.getText().toString().trim());
+
+            if (password.getText().toString().equals(confirmPassword.getText().toString()) && !password.getText().toString().matches("")) {
+                Toast.makeText(RegisterHere.this, "Account Created!", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(RegisterHere.this, MainActivity.class));
+            } else {
+                Toast.makeText(RegisterHere.this, "Confirm Password must match Password!", Toast.LENGTH_LONG).show();
+                password.getText().clear();
+                confirmPassword.getText().clear();
             }
 
-
-        });
-
-
-
-        createAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                register(username.getText().toString().trim(), email.getText().toString().trim(), phone.getText().toString().trim(),
-                        password.getText().toString().trim(), cpassword.getText().toString().trim());
-
-                if (password.getText().toString().equals(cpassword.getText().toString()) && !password.getText().toString().matches("")) {
-                    Toast.makeText(registerHere.this, "Account Created!", Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(registerHere.this, MainActivity.class));
-                } else {
-                    Toast.makeText(registerHere.this, "Confirm Password must match Password!", Toast.LENGTH_LONG).show();
-                    password.getText().clear();
-                    cpassword.getText().clear();
-                }
-
-                String nName = username.getText().toString();
-                String nEmail = email.getText().toString();
-                String nPhone = phone.getText().toString();
-                String nPassword = password.getText().toString();
+            String nName = username.getText().toString();
+            String nEmail = email.getText().toString();
+            String nPhone = phone.getText().toString();
+            String nPassword = password.getText().toString();
 
             SharedPreferenceEntry sharedPreferenceEntry = new SharedPreferenceEntry(nName, nEmail, nPhone, nPassword);
-                boolean isSuccess = mSharedPreferencesHelper.savePersonalInfo(sharedPreferenceEntry);
-                if (isSuccess) {
-                    Toast.makeText(registerHere.this, "Personal information saved", Toast.LENGTH_LONG).show();
-                    Log.i(TAG, "Personal information saved");
-                } else {
-                    Log.e(TAG, "Failed to write personal information to SharedPreferences");
-                }
+            boolean isSuccess = mSharedPreferencesHelper.savePersonalInfo(sharedPreferenceEntry);
+            if (isSuccess) {
+                Toast.makeText(RegisterHere.this, "Personal information saved", Toast.LENGTH_LONG).show();
+                Log.i(TAG, "Personal information saved");
+            } else {
+                Log.e(TAG, "Failed to write personal information to SharedPreferences");
             }
         });
 
 
     }
+
     private void populateUi() {
         SharedPreferenceEntry sharedPreferenceEntry;
         sharedPreferenceEntry = mSharedPreferencesHelper.getPersonalInfo();
@@ -133,30 +122,26 @@ public class registerHere extends AppCompatActivity {
             username.setError("Username is required");
             username.requestFocus();
             return;
-        } else if (!mEmailValidator.isValid()){
+        } else if (!mEmailValidator.isValid()) {
             email.setError("Invalid Email");
-            Log.w(TAG,"Not saving personal information: Invalid Email");
+            Log.w(TAG, "Not saving personal information: Invalid Email");
             email.requestFocus();
             return;
-        } else if (!mPhoneValidator.isValid()){
+        } else if (!mPhoneValidator.isValid()) {
             phone.setError("Invalid Phone Number");
-            Log.w(TAG,"Not saving personal information: Invalid Phone Number");
+            Log.w(TAG, "Not saving personal information: Invalid Phone Number");
             phone.requestFocus();
             return;
-        } else if (!mPasswordValidator.isValid()){
+        } else if (!mPasswordValidator.isValid()) {
             password.setError("Invalid Password");
-            Log.w(TAG,"Not saving personal information: Invalid Password");
+            Log.w(TAG, "Not saving personal information: Invalid Password");
             password.requestFocus();
             return;
         } else if (regCpassword.isEmpty()) {
-            cpassword.setError("Password is required");
-            cpassword.requestFocus();
+            confirmPassword.setError("Password is required");
+            confirmPassword.requestFocus();
             return;
         }
-
-        Toast toast = new Toast(getApplicationContext());
-        final TextView textView = (TextView) findViewById(R.id.text);
-// ...
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -164,7 +149,7 @@ public class registerHere extends AppCompatActivity {
 
         // Request a string response from the provided URL.
         StringRequest stringObjectRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            int statusCode = 0;
+            final int statusCode = 0;
 
             @Override
             public void onResponse(String response) {
@@ -186,13 +171,7 @@ public class registerHere extends AppCompatActivity {
                     Log.d("hey", "Error");
                 }
             }
-        }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                    Log.e("Response error", error.getMessage());
-            }
-        }) {
+        }, error -> Log.e("Response error", error.getMessage())) {
             @Override
             protected Response<String> parseNetworkResponse(NetworkResponse response) {
                 statusCode = response.statusCode;
@@ -214,14 +193,14 @@ public class registerHere extends AppCompatActivity {
                     jsonObject.put("phonenumber", regPhone);
                     jsonObject.put("password", regPassword);
 
-                    return jsonObject == null ? null : jsonObject.toString().getBytes("utf-8");
+                    return jsonObject == null ? null : jsonObject.toString().getBytes(StandardCharsets.UTF_8);
                 } catch (Exception e) {
                     return null;
                 }
             }
 
             @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
+            public Map<String, String> getHeaders() {
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("Content-Type", "application/json;");
 
@@ -229,8 +208,7 @@ public class registerHere extends AppCompatActivity {
             }
         };
 
-// Add the request to the RequestQueue.
+        // Add the request to the RequestQueue.
         queue.add(stringObjectRequest);
     }
-
 }
