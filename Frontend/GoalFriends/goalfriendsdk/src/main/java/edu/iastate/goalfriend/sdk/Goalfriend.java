c98204721e1b184651f63a296f8f7e90668243d3
Tyler Jaacks <TylerJaacks@Gmail.com>
2021-11-05 19:22:49 -0500

    Added SDK.

---
+++ Frontend/GoalFriends/goalfriendsdk/src/main/java/edu/iastate/goalfriend/sdk/Goalfriend.java
@@ -0,0 +1,16 @@
+package edu.iastate.goalfriend.sdk;
+
+import android.net.Uri;
+
+public class Goalfriend {
+    private static final String LOCAL_ENVIRONMENT = "http://localhost:8080";
+    private static final String PRODUCTION_ENVIRONMENT = "";
+
+    private Uri baseUri;
+
+    public Goalfriend() {
+
+    }
+
+    public
+}
