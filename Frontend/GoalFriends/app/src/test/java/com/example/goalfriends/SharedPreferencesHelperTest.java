3df5fc26ca625a36d52119fdef794f6565f2738f
Kyle Todd <kyletodd@iastate.edu>
2021-11-06 23:57:59 -0500

    Test

--- Frontend/GoalFriends/app/src/test/java/com/example/goalfriends/SharedPreferencesHelperTest.java
+++ Frontend/GoalFriends/app/src/test/java/com/example/goalfriends/SharedPreferencesHelperTest.java
@@ -7,11 +7,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+//import org.mockito.runners.MockitoJUnitRunner;
 import android.content.SharedPreferences;
-/**
- * Unit tests for the {@link SharedPreferencesHelper} that mocks {@link SharedPreferences}.
- */
+
 @RunWith(MockitoJUnitRunner.class)
 public class SharedPreferencesHelperTest {
     private static final String TEST_NAME = "Test name";
