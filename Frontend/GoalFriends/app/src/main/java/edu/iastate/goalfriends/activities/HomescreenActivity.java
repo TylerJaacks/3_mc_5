a964679dbbac84aabd99cacba3a505b6bd77f53e
Tyler Jaacks <TylerJaacks@Gmail.com>
2021-12-07 19:52:55 -0600

    Refactored a lot of shit.

--- Frontend/GoalFriends/app/src/main/java/edu/iastate/goalfriends/activities/HomescreenActivity.java
+++ Frontend/GoalFriends/app/src/main/java/edu/iastate/goalfriends/activities/HomescreenActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
 import com.android.volley.Request;
 import edu.iastate.goalfriends.R;
+import edu.iastate.goalfriends.threads.UpdateFriendGoalsListThread;
 import edu.iastate.goalfriends.threads.UpdateGoalListThread;
 
 import org.json.JSONObject;
@@ -23,54 +23,64 @@ import java.util.HashMap;
  * views that show the user's current goals and their friends recent activity.
  */
 public class HomescreenActivity extends AppCompatActivity {
+    private static final String userGoalsEndpoint = "http://coms-309-054.cs.iastate.edu:8080/goal/all";
+    private static final String friendGoalsEndpoint = "http://coms-309-054.cs.iastate.edu:8080/goal/all";
 
     private ImageButton addGoal;
     private ImageButton profile;
     private ImageButton search;
 
-    public static ArrayList<String> listItems = new ArrayList<>();
-    public static ArrayAdapter<String> adapter;
+    public static ArrayList<String> userGoalsArrayList = new ArrayList<>();
+    public static ArrayList<String> friendsGoalArrayList = new ArrayList<>();
+    public static ArrayAdapter<String> userGoalsAdapter;
+    public static ArrayAdapter<String> friendsGoalAdapter;
 
-    ListView goalListView;
+    ListView userGoalsListView;
+    ListView friendsGoalsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
-
         addGoal = (ImageButton) findViewById(R.id.AddGoalbutton);
         profile = (ImageButton) findViewById(R.id.Profilebutton);
         search = (ImageButton) findViewById(R.id.Searchbutton);
 
-        goalListView = (ListView) findViewById(R.id.rvList);
+        userGoalsListView = (ListView) findViewById(R.id.rvList);
+        friendsGoalsListView = (ListView) findViewById(R.id.friendsActivityListView);
 
-        adapter = new ArrayAdapter<>(this,
+        userGoalsAdapter = new ArrayAdapter<>(this,
                 R.layout.homescreen_listview,
-                listItems);
+                userGoalsArrayList);
 
-        goalListView.setAdapter(adapter);
+        friendsGoalAdapter = new ArrayAdapter<>(this,
+                R.layout.homescreen_listview,
+                friendsGoalArrayList);
+
+        userGoalsListView.setAdapter(userGoalsAdapter);
+        friendsGoalsListView.setAdapter(friendsGoalAdapter);
 
         updateAdapter();
-
-        String url = "http://coms-309-054.cs.iastate.edu:8080/goal/all";
 
         HashMap<String, String> headers = new HashMap<>();
         headers.put("token", MainActivity.token);
 
-        UpdateGoalListThread uglt = new UpdateGoalListThread(this, Request.Method.GET, url, new JSONObject(), new HashMap<>(), headers);
-        uglt.start();
+        UpdateGoalListThread updateUserGoalsListThread =
+                new UpdateGoalListThread (this, Request.Method.GET, userGoalsEndpoint, new JSONObject(), new HashMap<>(), headers);
+        updateUserGoalsListThread.start();
 
-        // TODO: Add click listeners to goal rows that way when users tap on a goal it brings them to a page where they can edit it
+        UpdateFriendGoalsListThread updateFriendGoalsListThread =
+                new UpdateFriendGoalsListThread(this, Request.Method.GET, friendGoalsEndpoint, new JSONObject(), new HashMap<>(), headers);
+        updateFriendGoalsListThread.start();
 
         addGoal.setOnClickListener(v -> startActivity(new Intent(HomescreenActivity.this, PostActivity.class)));
-
         profile.setOnClickListener(v -> startActivity(new Intent(HomescreenActivity.this, ProfileActivity.class)));
-
         search.setOnClickListener(v -> startActivity(new Intent(HomescreenActivity.this, SearchActivity.class)));
     }
 
     public static void updateAdapter(){
-        adapter.notifyDataSetChanged();
+        userGoalsAdapter.notifyDataSetChanged();
+        friendsGoalAdapter.notifyDataSetChanged();
     }
 }
